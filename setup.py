09767e81426150992755edfd4048dc51fb73905d
Francisco Gárate <fgaratesantiago@gmail.com>
2017-02-28 22:18:16 +0100

    submit pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 setup(
   name = 'pyliferisk',
-  packages = ['pyliferisk'], # this must be the same as the name above
+  packages = ['pyliferisk'],
   version = '1.4',
   description = 'A python library for life actuarial calculations, simple, powerful and easy-to-use.',
   author = 'Francisco Garate, Florian Pons',
   license='GPL-3.0',
-  url = 'https://github.com/franciscogarate/pyliferisk', # use the URL to the github repo
-  keywords = ['pyliferisk', 'life contingencies', 'actuarial'], # arbitrary keywords
-  classifiers = [],
+  url = 'https://github.com/franciscogarate/pyliferisk',
+  keywords = ['pyliferisk', 'life contingencies', 'actuarial'],
 )
