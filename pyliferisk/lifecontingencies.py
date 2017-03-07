8517ec44c8782cbcaf0bff81fcb6b5bd63b71b01
Francisco Gárate <fgaratesantiago@gmail.com>
2017-03-07 22:24:40 +0100

    submit pypi 1.8

--- pyliferisk/lifecontingencies.py
+++ pyliferisk/lifecontingencies.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #    pyrisk: A python library for simple actuarial calculations
-#    Version: 1.3 - October 2015
-#    Copyright (C) 2015 Francisco Garate
+#    Version: 1.8 - March 2017
+#    Copyright (C) 2017 Francisco Garate, Florian Pons
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -21,7 +21,7 @@
 
 # This class represente one mortality table
 # with basic actuarial values calculated (for optimisations)
-class MortalityTable:
+class LifeTable:
     def __init__(self, l_x=[], q_x=[], nt=None,perc=100):
         self.l_x = l_x
         self.q_x = q_x
@@ -85,7 +85,79 @@ class Pers:
             lx_g = self.l_x[g]
             sum_lx = sum_lx-lx_g
             self.e_x.append(0.5+sum_lx/lx_g) #[g+1:-2] according notes from ucm.es, [g+1:-1]
-        
+ 
+class MortalityTable:
+    def __init__(self, l_x=[], q_x=[], nt=None,i=None,perc=100):
+        self.lx = l_x
+        self.qx = q_x
+        self.dx = []
+        self.ex = []
+        self.w = 0
+        self.i = i 
+        self.q = 0        
+        self.perc = perc
+        self.nt = nt
+        self.Dx = []
+        self.Nx = []
+        self.Cx = []
+        self.Mx = []
+        self.nEx = []
+        if nt:
+            mt = nt
+            init = mt[0]
+            self.qx = [0.0]*init
+            end_val = 0
+            for val in mt[1:]:
+                if end_val < 1000.0:
+                    end_val = val*perc/100
+                    self.qx.append(end_val)
+            if perc != 100:
+                self.qx.append(1000)
+        if self.lx == []:
+            self.lx = [100000.0]
+            for val in self.qx:
+                self.lx.append(self.lx[-1]*(1-val/1000))
+        if self.lx[-1] != 0.0 : self.lx.append(0.0)
+        if self.w == 0 : self.w = self.lx.index(0)-1
+        if self.qx == []:
+            #self.qx = []
+            l_x = self.lx[0]
+            for l_x1 in self.lx[1:]:
+                self.qx.append((l_x-l_x1)*1000/l_x)
+                l_x = l_x1
+        if self.dx == []:
+            dx_0 = -1
+            end_x_lx = self.lx.index(0)
+            for lx0 in self.lx:
+                dx_0 +=1
+                lx1 = min(dx_0+1,end_x_lx)
+                self.dx.append(lx0-self.lx[lx1])
+        if self.ex == []:
+            for g in range(0,len(self.lx[:-1])):
+                lx_g = self.lx[g]
+                self.ex.append(0.5+sum(self.lx[g+1:-1])/lx_g) #[g+1:-2] according notes from ucm.es, [g+1:-1]
+        if self.Dx == []:
+            #self.Dx = []
+            age = -1
+            for j in self.lx:
+                age +=1
+                self.Dx.append(((1/(1+i))**age)*j)
+        if self.Nx == []:
+            #self.Nx = []
+            for k in range(0,len(self.Dx)):
+                self.Nx.append(sum(self.Dx[k:-1])) #[k:-2] according notes from ucm.es, [k:-1]
+        if self.Cx == []:
+            #self.Cx = []
+            age = -1
+            for l in self.dx:   #[:-1]
+                age +=1
+                C_x = ((1/(1+i))**(age+1))*l*((1+i)**0.5)
+                self.Cx.append(C_x)
+        if self.Mx == []:
+            #self.Mx = []
+            for m in range(0,len(self.Cx)):
+                self.Mx.append(sum(self.Cx[m:-1])) # [m:-2] according notes from ucm.es, [m:-1]
+
     # Actuarial notation -------------------
     def qx(self,x):
         """ qx: Returns the probability that a life aged x dies before 1 year
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pyliferisk',
   packages = ['pyliferisk'],
-  version = '1.4',
+  version = '1.8',
   description = 'A python library for life actuarial calculations, simple, powerful and easy-to-use.',
   author = 'Francisco Garate, Florian Pons',
   license='GPL-3.0',
