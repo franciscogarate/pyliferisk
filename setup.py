814626f5169c4ed0338b9c78c2124793a4813168
Mason Borda <mason@gen.life>
2017-04-18 16:17:57 -0700

    Format all python files to PEP8

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
+
 setup(
-  name = 'pyliferisk',
-  packages = ['pyliferisk'],
-  version = '1.8',
-  description = 'A python library for life actuarial calculations, simple, powerful and easy-to-use.',
-  author = 'Francisco Garate, Florian Pons',
-  license='GPL-3.0',
-  url = 'https://github.com/franciscogarate/pyliferisk',
-  keywords = ['pyliferisk', 'life contingencies', 'actuarial'],
+    name='pyliferisk',
+    packages=['pyliferisk'],
+    version='1.8',
+    description='A python library for life actuarial calculations, simple, powerful and easy-to-use.',
+    author='Francisco Garate, Florian Pons',
+    license='GPL-3.0',
+    url='https://github.com/franciscogarate/pyliferisk',
+    keywords=['pyliferisk', 'life contingencies', 'actuarial'],
 )
